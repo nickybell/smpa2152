8bfe30b2a031c5017a97091a6d8d195909fdbd57
Nicky Bell <107280719+nickybell@users.noreply.github.com>
2024-09-16 16:20:55 -0400

    data visualization (#2)
    * adds exercise for evaluating graphs
    
    * change exercise to two column format
    
    * updates to assignment 1
    
    * initial commit of teaching script
    
    * adds exercise for evaluating graphs
    
    * change from gun violence example to trump support example
    
    * complete data visualization scripts
    
    * add data viz lecture materials
    
    * remove unnecessary .zzz files
    
    * updates to assignment 1
    
    * adds assignment 2

---
+++ assignments/assignment1/assignment1_template.R
@@ -0,0 +1,70 @@
+#----------------------------------------------
+# SMPA 2152
+# Assignment #1
+# Name: YOUR NAME HERE
+# Worked with: NAMES HERE (if applicable)
+#----------------------------------------------
+
+# Please type your code into the sections outlined below. When a question asks you to interpret the results of your code, use # to write your answer as a comment.
+
+# You will also need to submit a Word document or PDF containing the visualizations for questions 5 and 6.
+
+#----------------------------------------------
+# Question 1
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 2
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 3
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 4
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 5
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 6
+
+
+
+
+
+
+
+
---
+++ assignments/assignment2/assignment2_template.R
@@ -0,0 +1,44 @@
+#----------------------------------------------
+# SMPA 2152
+# Assignment #2
+# Name: YOUR NAME HERE
+# Worked with: NAMES HERE (if applicable)
+#----------------------------------------------
+
+# Please type your code into the sections outlined below.
+
+# You will also need to submit a Word document or PDF containing the visualizations you create along with your written responses to the prompts.
+
+#----------------------------------------------
+# Question 1
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 2
+
+
+
+
+
+
+
+
+#----------------------------------------------
+# Question 3
+
+library(dplyr)
+house_prices |>
+  mutate(month = as.numeric(format(date, "%m"))) |>
+  count(month) |>
+  ggplot()
+
+
+
+
+
+
